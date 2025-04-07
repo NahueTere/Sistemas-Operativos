3a7ea4d31ceecc7d850361f05bdab16c76823057
NahueTere <nahuelterenzanito@gmail.com>
2025-04-07 10:09:44 -0300

    Add files via upload

---
+++ del 6 al 10.py
@@ -0,0 +1,39 @@
+import math
+"""Ejercicio 6
+ay = float(input("Ingrese el valor de y de a (mm):"))
+ax = float(input("Ingrese el valor de x de a (mm):"))
+by = float(input("Ingrese el valor de y de b (mm):"))
+bx = float(input("Ingrese el valor de x de b (mm):"))
+D = round(((((ay-by)**2)+((ax-bx)**2))**0.5),2)
+print("la distancia entre a y b es de:",D,"mm")"""
+
+"""Ejercicio 7
+n1 = float(input("Primer numero"))
+n2 = float(input("Segundo numero"))
+p = (n1+n2)/2
+print("Promedio:",p)"""
+
+"""Ejercicio 8
+a = float(input("Altura (mm): "))
+b = float(input("Base (mm): "))
+p = a*2+b*2
+area = a*b
+print("El perimetro es de",p,"mm")
+print("El area es de",area,"mm²")"""
+
+"""Ejercio 9
+galon = 3.785
+precio = 1200
+gs = float(input("Galones surtidos: "))
+l = gs*galon
+pt = l*precio
+print(f"El total a pagar es de ${pt}")"""
+
+"""Ejercio 10
+pi = 3.1416
+h = float(input("Altura (mm): "))
+r = float(input("Radio (mm): "))
+A = 2*pi*r*h + 2*pi*r**2
+V = (2*pi*r**2)*h
+print("El area del cilindrio es de:",A,"mm")
+print("El volumen es de:",round(V,1),"mm³")"""

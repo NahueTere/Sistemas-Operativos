630cb6000b5af24017360b9bb78c7a37dfa7fcb3
NahueTere <nahuelterenzanito@gmail.com>
2025-04-14 09:16:53 -0300

    Update ejercicios.py

--- Ejercicios Complementarios/ejercicios.py
+++ Ejercicios Complementarios/ejercicios.py
@@ -1,5 +1,29 @@
 #Ejercicio 1
-r = int(input("Ingrese radio de la esfera (mm): "))
-pi = 3.1416
-v = (4/3)*pi*(r**3)
-print(f"Volumen= {round(v,2)}mm")
+#r = int(input("Ingrese radio de la esfera (mm): "))
+#pi = 3.1416
+#v = (4/3)*pi*(r**3)
+#print(f"Volumen= {round(v,2)}mm")
+
+#Ejercicio 2
+#h = int(input("Ingrese altura del triangulo (mm): "))
+#p = 3*(2*h/3**0.5)
+#print(f"Perimetro= {round(p,2)}mm")
+
+#Ejercicio 3
+#soles = float(input("Cuantos soles tenes?: "))
+#d = soles/3.25
+#e = soles/3.83
+#print(f"Equivalente en dolares: {d}")
+#print(f"Equivalente en euros: {e}")
+
+#Ejercicio 4
+#m = float(input("Metros: "))
+#s = float(input("Segundos: "))
+#v = m/s
+#print(f"{round(v,2)}m/s")
+
+#Ejercicio 5
+p = float(input("Precio por unidad: "))
+d = int(input("Cantidad de docenas: "))
+pt = d*12*p
+print(f"Precio a pagar: {pt}")
